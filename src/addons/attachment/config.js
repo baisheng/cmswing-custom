6a8abbefb9060f38e07add846eb659bffef6748a
baisheng@gmail.com <baisheng@gmail.com>
2018-07-16 15:51:25 +0800

    update

--- src/addons/attachment/config.js
+++ src/addons/attachment/config.js
@@ -5,7 +5,7 @@ module.exports = {
   isadm: 1, // 是否有后台管理，1：有，0：没有，入口地址:'/ext/attachment/admin/index'
   isindex: 0, // 是否需要前台访问，1：需要，0：不需要,入口地址:'/ext/attachment/index/index'
   version: '1.0', // 版本号
-  author: 'CaiXie', // 作者
+  author: 'CmsWing', // 作者
   table: ['attachment', 'attachment_file', 'attachment_pic'], // 插件包含的 数据库表，不包含表前缀，如：cx_ext_table 就是 table，多个['table','table_2']没有留空数组。
   sql: '', // 插件安装的时候会找个名字的sql文件导入，默认 插件目录名.sql;
   hooks: ['homeUpPic', 'homeUpPics', 'homeUpFile', 'adminUpPic', 'adminUpPics', 'adminUpFile', 'adminUpHtmlFile'], // 挂载的钩子，数组格式，如['hooks1', 'hooks2'],不挂载留空：[]
